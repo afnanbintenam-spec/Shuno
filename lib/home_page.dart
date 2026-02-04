e975a6230aaf8cf95a976271a3c891209ee24365
afnanbintenam-spec <afnanbintenam-spec@users.noreply.github.com>
2026-02-04 22:53:20 +0600

    Fix image asset path

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:shuno/Pallete.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +16,33 @@ class HomePageState extends State<HomePage> {
         title: const Text('Shuno'),
         centerTitle: true,
         leading: const Icon(Icons.menu),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+
+          // Avatar Circle Frame
+          Center(
+            child: Container(
+              height: 160,
+              width: 160,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Pallete.assistantCircleColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(18),
+                child: ClipOval(
+                  child: Image.asset(
+                    'Assets/virtualAssistant.png',
+                    fit: BoxFit.contain,
+                    alignment: Alignment.bottomCenter,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
